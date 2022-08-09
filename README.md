42054da8a5a13bd765f195af48a161dcb3818168
kpaveliev <kpaveliev@gmail.com>
2022-08-09 13:45:26 +0300

    csv_to_json script updated

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SkyPro.Python course
 ## HW27. Avito - simple Django app
 
-**Prepare data**
+**Create fixtures and load data**
 
-1. Move to folder "data"
-2. Execute 'csv_to_json.py' script (gets data from csv files, converts into json and puts into fixtures folder)
+1. Move to folder "data" (csv files with data are located there)
+2. Execute 'python3 csv_to_json.py' (gets data from csv files, converts into json and puts into fixtures folder)
+3. Return to project folder and execute 'python3 manage.py loaddata ads.json'
+4. Execute 'python3 manage.py loaddata categories.json'
--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -3,15 +3,15 @@ import csv, json
 
 # Params
 csv_file_ads = 'ads.csv'
-json_file_ads = '../fixtures/ads.json'
+json_file_ads = '../ads/fixtures/ads.json'
 ads_model = 'ads.ad'
 
 csv_file_catetories = 'categories.csv'
-json_file_categories = '../fixtures/categories.json'
+json_file_categories = '../ads/fixtures/categories.json'
 categories_model = 'ads.category'
 
 
-# Function
+# Functions
 def csv_to_json(csv_file_path: str, json_file_path: str, model: str) -> str:
     """Convert csv to json"""
 
@@ -20,8 +20,9 @@ def csv_to_json(csv_file_path: str, json_file_path: str, model: str) -> str:
         reader = csv.DictReader(file)
         data: list = [
             {'model': model,
-             'pk': int(row['id']),
-             'fields': row
+             'pk': int(row['id']) if row.get('id') else int(row['Id']),
+             'fields': {
+                 key: replace_values(value) for key, value in row.items() if key != 'id' and key != 'Id'}
              }
             for row in reader
         ]
@@ -33,6 +34,17 @@ def csv_to_json(csv_file_path: str, json_file_path: str, model: str) -> str:
     return f'Data from csv ({csv_file_path}) converted to json ({json_file_path})'
 
 
+def replace_values(value):
+    """Convert digits and bool values into required types"""
+    if value.isdigit():
+        return int(value)
+    if value == 'TRUE' or value == 'FALSE':
+        return bool(value)
+
+    return value
+
+
 if __name__ == '__main__':
     print(csv_to_json(csv_file_ads, json_file_ads, ads_model))
     print(csv_to_json(csv_file_catetories, json_file_categories, categories_model))
+
