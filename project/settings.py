ba8dd7786ca3f596a9d146be779cbac0460ae86d
kpaveliev <kpaveliev@gmail.com>
2022-08-30 19:00:57 +0300

    jwt authentication added

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'ads',
     'users'
 ]
@@ -140,6 +141,9 @@ TOTAL_ON_PAGE = 10
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
 }
 
 # Change auth model
--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ from rest_framework import routers
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ads.urls')),
-    path('', include('users.urls')),
+    path('user/', include('users.urls')),
 ]
 
 # To allow image display
